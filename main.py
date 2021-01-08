7f6a61df1bc83b8c8363d644c89eb0001cded24c
Felipew34 <felipew34@alu.ufc.br>
2021-01-08 12:27:45 +0000

    Adicionei exercício, eu não sei como fazer aparecer o ano no print, já fiz das maneiras que sei e que deram certo, mas não aparece.

--- main.py
+++ main.py
@@ -4,8 +4,14 @@
 def main():
     """Função principal que será rodada quando o script for passado para o interpretador."""
     # COLOQUE SEU CÓDIGO AQUI
-    # TODO
-
+    a = int(input("Ano:"))
+    if a%4==0 and a%100!=0:
+      print(f"O ano [a] é bissexto.")
+    elif a%400==0:
+      print(f"O ano [a] é bissexto.")
+    else:
+      print(f"O ano [a] não é bissexto.")      
+            
 
 if __name__ == '__main__':
     main()
