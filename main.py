80742dc53d6ddf2d341c1c8d1a5a68735c708c83
Felipew34 <felipew34@alu.ufc.br>
2021-01-08 14:24:22 +0000

    Agora deu certo

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ def main():
     """Função principal que será rodada quando o script for passado para o interpretador."""
     # COLOQUE SEU CÓDIGO AQUI
     a = int(input("Ano:"))
-    if a%4==0 and a%100!=0:
-      print(f"O ano [a] é bissexto.")
-    elif a%400==0:
-      print(f"O ano [a] é bissexto.")
+    if a % 4== 0 and a % 100!= 0:
+      print(f"O ano {a} é bissexto.")
+    elif a % 400== 0:
+      print(f"O ano {a} é bissexto.")
     else:
-      print(f"O ano [a] não é bissexto.")      
+      print(f"O ano {a} não é bissexto.")      
             
 
 if __name__ == '__main__':
